45ba26ae899865893cfdc9aaaeb3e63870a21f93
mbaxter <github@phozop.net>
2012-01-25 17:07:10 -0500

    Update

--- src/to/joe/permtest/PermTest.java
+++ src/to/joe/permtest/PermTest.java
@@ -1,39 +1,55 @@
 package to.joe.permtest;
 
-import java.util.logging.Logger;
-
-import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PermTest extends JavaPlugin {
 
-    private PluginDescriptionFile selfDescription;
-
-    private Logger log;
-
-
-    /**
-     * Tag that log!
-     * 
-     * @param message
-     */
-    public void log(String message) {
-        this.log.info("[PermTest] " + message);
-    }
-
     @Override
     public void onDisable() {
-        this.log("Version " + this.selfDescription.getVersion() + " disabled.");
+        this.getLogger().info("Version " + this.getDescription().getVersion() + " disabled.");
     }
 
     @Override
     public void onEnable() {
-        this.selfDescription = this.getDescription();
+        this.getLogger().info("Version " + this.getDescription().getVersion() + " enabled.");
+    }
 
-        this.log = Logger.getLogger("Minecraft");
-
-        this.getCommand("permtest").setExecutor(new PermTestCommand());
-
-        this.log("Version " + this.selfDescription.getVersion() + " enabled.");
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        final StringBuilder message = new StringBuilder();
+        String permission;
+        message.append(ChatColor.DARK_AQUA);
+        if (args.length == 1) {
+            permission = args[0];
+            message.append("You");
+            if (!sender.hasPermission(permission)) {
+                message.append(" do not");
+            }
+            message.append(" have ");
+        } else if (args.length > 1) {
+            final Player target = Bukkit.getServer().getPlayer(args[0]);
+            if (target == null) {
+                message.append("Cannot find player: " + ChatColor.AQUA + args[1]);
+                sender.sendMessage(message.toString());
+                return true;
+            }
+            message.append("Player " + ChatColor.AQUA + target.getName() + ChatColor.DARK_AQUA);
+            permission = args[1];
+            if (!target.hasPermission(permission)) {
+                message.append(" does not have ");
+            } else {
+                message.append(" has ");
+            }
+        } else {
+            return false;
+        }
+        message.append(ChatColor.AQUA + permission);
+        sender.sendMessage(message.toString());
+        return true;
     }
 }
